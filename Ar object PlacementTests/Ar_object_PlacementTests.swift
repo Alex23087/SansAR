204585cc9487a52a4491ecbabdcba78012c50209
Alessandro Parisi <alessandroparisi@10-20-32-135.0100.fed.it.iosda.org>
2018-11-20 16:54:20 +0100

    Initial Commit

---
+++ Ar object PlacementTests/Ar_object_PlacementTests.swift
@@ -0,0 +1,34 @@
+//
+//  Ar_object_PlacementTests.swift
+//  Ar object PlacementTests
+//
+//  Created by Alessandro Parisi on 20/11/2018.
+//  Copyright © 2018 Alessandro Parisi. All rights reserved.
+//
+
+import XCTest
+@testable import Ar_object_Placement
+
+class Ar_object_PlacementTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
